3c5010203769f30779e369401244fd38eb8bb1ce
imomi0824 <imomi0824@gmail.com>
2026-04-06 22:11:22 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'money-v2-1.7';
+const CACHE = 'money-v2-2.3';
 const URLS = [
   './', './index.html', './manifest.json', './icon.png',
   'https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js'
